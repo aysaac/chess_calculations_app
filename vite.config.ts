c28d596f65c8c73ca775b15295f2c532c85bd98f
Isaac Gutierrez <isaacgr2014@hotmail.com>
2026-05-02 17:12:37 -0600

    Fix asset paths
    Fix asset paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
 export default defineConfig({
-  plugins: [cloudflare()],
+  base: '/chess-calc/',
+  plugins: [],
   build: {
     rollupOptions: {
       input: {
