e9e3d6bab7f0805251fd228b3327eee8790ebfdc
Isaac Gutierrez <isaacgr2014@hotmail.com>
2026-05-03 18:32:46 -0600

    Updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/chess-calc/',
+  base: '/',
   plugins: [],
   build: {
     rollupOptions: {
